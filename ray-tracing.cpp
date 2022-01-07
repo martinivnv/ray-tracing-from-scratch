5b95df8855a1b0b422041313400034432e8e5c04
martinivnv <martinivnv2002@gmail.com>
2022-01-07 11:11:33 -0500

    Added progress indicator

--- ray-tracing.cpp
+++ ray-tracing.cpp
@@ -14,6 +14,7 @@ int main() {
     cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = image_height-1; j >= 0; --j) {
+        cerr << "\rScanlines remaining: " << j << ' ' << flush;
         for (int i = 0; i < image_width; ++i) {
             auto r = double(i) / (image_width-1);
             auto g = double(j) / (image_height-1);
@@ -26,4 +27,5 @@ int main() {
             cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+    cerr << "\nDone.\n";
 }

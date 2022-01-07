431dd30bfcc01ac26c865d1a359db246d6b8cf78
martinivnv <martinivnv2002@gmail.com>
2022-01-07 11:47:01 -0500

    Include our new header files in the main file

--- ray-tracing.cpp
+++ ray-tracing.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
+#include "color.h"
+#include "vec3.h"
 
-using namespace std;
+#include <iostream>
 
 int main() {
     
@@ -11,21 +12,14 @@ int main() {
     // Render
     // P3 means colors are in ASCII
     // 255 is the max color
-    cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = image_height-1; j >= 0; --j) {
-        cerr << "\rScanlines remaining: " << j << ' ' << flush;
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
-            auto b = 0.25;
-
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-
-            cout << ir << ' ' << ig << ' ' << ib << '\n';
+            color pixel_color(double(i)/(image_width-1), double(j)/(image_height-1), 0.25);
+            write_color(std::cout, pixel_color);
         }
     }
-    cerr << "\nDone.\n";
+    std::cerr << "\nDone.\n";
 }

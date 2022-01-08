7d700974924ec04dca236c480d6b36079703aaa0
martinivnv <martinivnv2002@gmail.com>
2022-01-07 23:42:04 -0500

    Added multithreading ability

--- ray-tracing.cpp
+++ ray-tracing.cpp
@@ -1,38 +1,78 @@
-#include "rtweekend.h"
+#include <iostream>
+#include <fstream>		// to write results to ppm file
+#include <atomic>		// to count number of completed threads
+#include <chrono>		// to record start and end time and calculate elapsed time
+#include <thread>		// split up image into rows and trace in threads
+#include <vector>		// used for storing worker threads
+#include <mutex>		// lock for some cout stuff that occurs at the end of a thread just before join()
 
+#include "rtweekend.h"
 #include "color.h"
 #include "hittable_list.h"
 #include "sphere.h"
 #include "camera.h"
 #include "material.h"
 
-#include <iostream>
+using namespace std;
+using namespace chrono;
 
-color ray_color(const ray& r, const hittable& world, int depth) {
-    hit_record rec;
+static mutex coutMutex;
 
-    // If we've exceeded the ray bounce limit, no more light is gathered
-    if (depth <= 0)
-        return color(0,0,0);
+// Image
+const auto aspect_ratio = 3.0 / 2.0;
+const int image_width = 600;
+const int image_height = static_cast<int>(image_width / aspect_ratio);
+const int samples_per_pixel = 250;
+const int max_depth = 50;
 
-    if (world.hit(r, 0.001, infinity, rec)) {
-        ray scattered;
-        color attenuation;
-        if (rec.mat_ptr->scatter(r, rec, attenuation, scattered))
-            return attenuation * ray_color(scattered, world, depth-1);
-        return color(0,0,0);
-    }
-    vec3 unit_direction = unit_vector(r.direction());
-     auto t = 0.5*(unit_direction.y() + 1.0);
-    // Linearly blends white and blue 
-    // Scaled to 0.0 <= t <= 1.0, where 0.0 is white and 1.0 is blue 
-    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
+// Camera
+point3 lookfrom(14,1.5,-5);
+point3 lookat(0,0.5,0);
+vec3 vup(0,1,0);
+auto dist_to_focus = (lookfrom-lookat).length();
+auto aperture = 0.1;
+
+camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
+
+// Multithreading
+atomic<int> jobsDone{ 0 };
+atomic<int> nextJob{ 0 };
+const int numThreadsScalar = 3;
+
+// Initialise world and pixelBuffer to null
+hittable_list world;
+int* pixelBuffer = nullptr;
+
+color ray_color(const ray& r, hittable_list world, int depth)
+{
+	hit_record record;
+
+	if (world.hit(r, 0.001, infinity, record))
+	{
+		ray scattered;
+		color attenuation;
+
+		if (depth < max_depth && record.mat_ptr->scatter(r, record, attenuation, scattered))
+		{
+			return attenuation * ray_color(scattered, world, depth + 1);
+		}
+		else
+		{
+			return color(0, 0, 0);
+		}
+	}
+	else // hit background, gradient from white to light blue.
+	{
+		vec3 unit_direction = unit_vector(r.direction());
+		double t = 0.5 * unit_direction.y() + 1.0;
+		return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+	}
 }
 
 hittable_list random_scene() {
     hittable_list world;
 
-    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+    auto ground_material = make_shared<lambertian>(color(0.11, 0.11, 0.11));
     world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
 
     for (int a = -11; a < 11; a++) {
@@ -64,55 +104,111 @@ hittable_list random_scene() {
     }
 
     auto material1 = make_shared<dielectric>(1.5);
-    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material1));
 
-    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
-    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+    auto material2 = make_shared<lambertian>(color(0.15, 0.6, 0.5));
+    world.add(make_shared<sphere>(point3(-4, 1, -0.4), 1.0, material2));
 
-    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
-    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+    auto material3 = make_shared<metal>(color(0.90, 0.32, 0.64), 0.0);
+    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material3));
 
     return world;
 }
 
+void WritePpmFile(int* buffer, int width, int height, const string fileName)
+{
+	ofstream outFile;
+	outFile.open(fileName);
+
+	outFile << "P3\n" << width << " " << height << "\n255\n";
+
+	int length = width * height * 3;
+
+	for (int i = 0; i < length; i += 3)
+	{
+		outFile << buffer[i] << " " << buffer[i + 1] << " " << buffer[i + 2] << "\n";
+	}
+
+	outFile.close();
+}
+
+void trace()
+{
+	do
+	{
+		int y = nextJob++;
+
+		int index = ((image_height - 1) - y) * image_width * 3;
+		double oneOverWidth = 1.0 / image_width;
+		double oneOverHeight = 1.0 / image_height;
+
+		for (int i = 0; i < image_width; ++i)
+		{
+            color col(0, 0, 0);
+			for (int s = samples_per_pixel - 1; s >= 0; --s)
+			{
+				double u = double(i + random_double()) * oneOverWidth;
+				double v = double(y + random_double()) * oneOverHeight;
+				ray r = cam.get_ray(u, v);
+				col += ray_color(r, world, 0);
+			}
+
+			col /= double(samples_per_pixel);
+			col = color(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
+
+			pixelBuffer[index++] = int(255.99f * col[0]);
+			pixelBuffer[index++] = int(255.99f * col[1]);
+			pixelBuffer[index++] = int(255.99f * col[2]);
+		}
+
+		++jobsDone;
+
+		if (jobsDone % 10 == 0)
+		{
+			lock_guard<mutex> lock(coutMutex);
+			cout << jobsDone << "\\" << image_height << " jobs finished." << endl;
+		}
+	} while (nextJob < image_height);
+}
+
 int main() {
     
-    // Image
-    const auto aspect_ratio = 3.0 / 2.0;
-    const int image_width = 400;
-    const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 100;
-    const int max_depth = 50;
+    // Generate world
+    world = random_scene();
 
-    // World
-    auto world = random_scene();
+    time_point<steady_clock> start = steady_clock::now();
 
-    // Camera
-    point3 lookfrom(13,2,3);
-    point3 lookat(0,0,0);
-    vec3 vup(0,1,0);
-    auto dist_to_focus = 10.0;
-    auto aperture = 0.1;
+	pixelBuffer = new int[image_width * image_height * 3];
 
-    camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
+	unsigned int numThreads = thread::hardware_concurrency() * numThreadsScalar;
+	vector<thread> workers;
 
-    // Render
-    // P3 means colors are in ASCII
-    // 255 is the max color
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+	unsigned int nextThreadIndex = 0;
+	while (nextThreadIndex < numThreads && nextThreadIndex < image_height)
+	{
+		workers.emplace_back(thread(trace));
+		++nextThreadIndex;
+	}
 
-    for (int j = image_height-1; j >= 0; --j) {
-        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
-        for (int i = 0; i < image_width; ++i) {
-            color pixel_color(0, 0, 0);
-            for (int s = 0; s < samples_per_pixel; ++s) {
-                auto u = (i + random_double()) / (image_width-1);
-                auto v = (j + random_double()) / (image_height-1);
-                ray r = cam.get_ray(u, v);
-                pixel_color += ray_color(r, world, max_depth);
-            }
-            write_color(std::cout, pixel_color, samples_per_pixel);
-        }
-    }
-    std::cerr << "\nDone.\n";
+	numThreads = (int)workers.size();
+	{
+		lock_guard<mutex> lock(coutMutex);
+		cout << numThreads << " workers launched to complete " << image_height << " jobs." << endl;
+	}
+	
+	for (auto& worker : workers)
+	{
+		worker.join();
+	}
+
+	duration<double> elapsedSeconds = steady_clock::now() - start;
+	cout << "Elapsed time: " << elapsedSeconds.count() << " seconds" << endl;
+
+	string fileName = "";
+	cout << "Finished. The output will be saved in the given file." << endl;
+	cout << "Please enter the filename (without extension): ";
+	getline(cin, fileName);
+	string s = string(fileName) + string(".ppm");
+	WritePpmFile(pixelBuffer, image_width, image_height, s);
+	delete[] pixelBuffer;
 }
